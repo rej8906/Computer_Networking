447c8bb20038fe21a56ba83e1eec2d8c2d0b0011
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-20 16:54:22 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,27 +1,62 @@
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
+ #import socket module
+from socket import *
+import sys # In order to terminate the program
 
-def welcome_assignment_answers(question):
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question ==  "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
-    return(answer)
+def webServer(port=13331):
+
+   serverName = 'RobsServer'
+
+   serverPort = 13331
+
+   serverSocket = socket(AF_INET, SOCK_STREAM)
+
+   serverSocket.bind((serverName, serverPort))
+
+   serverSocket.listen(1)  #Prepare a server socket
+
+   #Fill in start
+
+   #Fill in end
+
+   while True:
+       #Establish the connection
+       #print('Server socket is ready to receive:')
+       connectionSocket, addr = serverSocket.accept()
+       connectionSocket.recv(13331).decode()
+
+       #Fill in start      #Fill in end
+       try:
+           message = serverSocket.recv(13331)
+           filename = message.split()[1]
+           f = open(filename[1:])
+           outputdata = f
+
+           #Send one HTTP header line into socket
+           #Fill in start
+
+           #Fill in end
+
+           #Send the content of the requested file to the client
+           for i in range(0, len(outputdata)):
+               connectionSocket.send(outputdata[i].encode())
+
+           connectionSocket.send("\r\n".encode())
+           #print('OK (200)')
+     
+       except IOError:
+           #print('file not found (404)')
+           #Send response message for file not found (404)
+           #Fill in start
+
+           #Fill in end
+
+           #Close client socket
+           #Fill in start
+
+           #Fill in end
+   connectionSocket.close()
+   serverSocket.close()
+   sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
-    print(welcome_assignment_answers(debug_question))
+   webServer(13331)
