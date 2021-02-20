42e6b0cd0cc95dedd7d70deeb4334649a980ca53
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-20 18:03:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -26,7 +26,7 @@ def webServer(port=13331):
 
         # Fill in start      #Fill in end
         try:
-            message = b "test message\n"
+            message = b "hello welcome to the thunderdome"
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f
