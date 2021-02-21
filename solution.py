faa6b47c9c7a012b2280c29e3688815c6f9bed1e
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-20 20:21:30 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def webServer(port=13331):
 
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
-    serverSocket.bind((serverName, serverPort))
+    serverSocket.bind(('', serverPort))
 
     serverSocket.listen(1)  # Prepare a server socket
 
