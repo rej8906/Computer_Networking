7ff704ce2c73103a9c109b9b012fb69ff0e4c030
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-20 20:29:26 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -24,7 +24,7 @@ def webServer(port=13331):
 
         # Fill in start      #Fill in end
         try:
-            message = b'test message here'
+            message = serverSocket.recvfrom(2048).decode()
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f.read()
