a8fa53b1ee721c383ff4c40cb3298d167de88fcf
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-20 22:27:31 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -38,6 +38,7 @@ def webServer(port=13331):
 
             # Fill in end
             for i in range(0, len(outputdata)):
+                connectionSocket.sendall(outputdata[i].encode())
                 connectionSocket.sendall("\r\n\r\n".encode())
                 connectionSocket.close()
             connectionSocket.close()
