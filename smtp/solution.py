d6a5a8eddf6224e132ba1965f8ef6d2912d7a8ef
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-27 12:25:37 -0500

    Update solution.py

--- smtp/solution.py
+++ smtp/solution.py
@@ -9,7 +9,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    clientSocket = socket(AF_INET, SOCK_STREAM)
    clientSocket.connect((mailserver)) # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
-   #recv = clientSocket.recv(1024).decode()
+   recv = clientSocket.recv(1024).decode()
    #print(recv)
    #if recv[:3] != '220':
       #print('220 reply not received from server.')
