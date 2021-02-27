26fec512450e385b19939c8937d92c164df68ab0
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-02-27 14:48:33 -0500

    Update solution.py

--- smtp/solution.py
+++ smtp/solution.py
@@ -1,8 +1,9 @@
 from socket import *
 
 def smtp_client(port=1025, mailserver='127.0.0.1'):
-    msg = '\r\n My message'
-    endmsg = '\r\n.\r\n'
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
+
 
     mailserver = ('127.0.0.1', 1025) # Choose a mail server (e.g. Google mail server)
 
