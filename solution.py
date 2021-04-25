93af4d65579fde363ae03bd00c0a30e17b8c2b20
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-04-24 23:13:28 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -58,7 +58,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         type, code, checksum, id, seq = struct.unpack('bbHHh', recPacket[20:28])
         bytesInDouble = struct.calcsize('d')
         timeData = struct.unpack('d', recPacket[28:28 + bytesInDouble])[0]
-        delay = (timeReceived - timeData)
+        delay = (timeReceived - timeData) * 1000
         packet_min = min(packet_min, delay)
         packet_max = max(packet_max, delay)
         stdev_var[packet_cnt] = delay ;
@@ -130,4 +130,4 @@ def ping(host, timeout=1):
     return vars
 
 if __name__ == '__main__':
-    print(ping("google.co.il"))
+    ping("google.co.il")
