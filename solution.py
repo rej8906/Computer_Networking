c6887f169ef7687db8dcb0ad5d53f28edfcdefec
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-04-26 17:06:00 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -49,7 +49,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         howLongInSelect = (time.time() - startedSelect)
         if whatReady[0] == []:  # Timeout
             return "Request timed out."
-        timeReceived = time.time()
+        timeReceived = (time.time()) * 10
         recPacket, addr = mySocket.recvfrom(1024)
         # Fill in start
         # Fetch the ICMP header from the IP packet
@@ -57,7 +57,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         icmpHeader = recPacket[20:28]
         type, code, checksum, id, seq = struct.unpack('bbHHh', recPacket[20:28])
         bytesInDouble = struct.calcsize('d')
-        timeData = struct.unpack('d', recPacket[28:28 + bytesInDouble])[0]
+        timeData = (struct.unpack('d', recPacket[28:28 + bytesInDouble])[0]) * 10
         delay = (timeReceived - timeData) * 100
         packet_min = min(packet_min, delay)
         packet_max = max(packet_max, delay)
