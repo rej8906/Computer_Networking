5204b7b96d81b4453e33c98458d1d9ab4922c79b
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-04-26 17:26:59 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -59,7 +59,6 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         bytesInDouble = struct.calcsize('d')
         timeData = (struct.unpack('d', recPacket[28:28 + bytesInDouble])[0])
         delay = (timeReceived - timeData)
-        delay = round((delay * 1000), 2)
         packet_min = min(packet_min, delay)
         packet_max = max(packet_max, delay)
         stdev_var[packet_cnt] = delay ;
@@ -118,25 +117,25 @@ def ping(host, timeout=1):
     global delay, packet_max, packet_min, packet_avg, stdev_var
     #timeout=1 means: If one second goes by without a reply from the server, # the client assumes that either the client's ping or the server's pong is lost
     dest = gethostbyname(host)
-    vars = [] * 16
-    #print("Pinging " + dest + " using Python:")
-    #print("")
+    vars = []
+    print("Pinging " + dest + " using Python:")
+    print("")
     # Calculate vars values and return them
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
-        #print(delay)
+        print(delay)
         time.sleep(1) #change me to 1
 
-    vars = [str(round(packet_min , 2)), str(round(packet_avg , 2)), str(round(packet_max , 2)), str(round((stdev(stdev_var)), 2))] # change me to 2
-    #print("testvals:")
-    #print("min", packet_min)
-    #print("avg" , packet_avg)
-    #print("max" , packet_max)
+    #vars = [str(round(packet_min , 2)), str(round(packet_avg , 2)), str(round(packet_max , 2)), str(round((stdev(stdev_var)), 2))] # change me to 2
+    print("testvals:")
+    print("min", packet_min)
+    print("avg" , packet_avg)
+    print("max" , packet_max)
     #print("stdev" , ((stdev(stdev_var))))
 
-    #vars.append(str(round(packet_min , 2)))
-    #vars.append(str(round(packet_avg , 2)))
-    #vars.append(str(round(packet_max , 2)))
-    #vars.append(str(round((stdev(stdev_var)) ,2)))
+    vars.append(str(round(packet_min , 2)))
+    vars.append(str(round(packet_avg , 2)))
+    vars.append(str(round(packet_max , 2)))
+    vars.append(str(round((stdev(stdev_var)) ,2)))
     return vars
