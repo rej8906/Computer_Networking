7b349cfffe8ba7c870ba09834b9ce228c7af88b5
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-05-02 18:21:42 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -129,9 +129,9 @@ def ping(host, timeout=1):
 
     #vars = [float(round(packet_min , 2)), float(round(packet_avg , 2)), float(round(packet_max , 2)), float(round((stdev(stdev_var)), 2))] # change me to 2
     vars.append(str(round(packet_min)))
-    vars.append(str(round(packet_avg , 2)))
+    vars.append(str(round(packet_avg , 1)))
     vars.append(str(round(packet_max)))
-    vars.append(str(round((stdev(stdev_var)) ,2)))
+    vars.append(str(round((stdev(stdev_var)) ,1)))
     print(vars)
     return vars
 
