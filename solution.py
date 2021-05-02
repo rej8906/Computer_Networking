42cd2a532ef3f7df8a3c3209c365452e4ccfd3ed
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-05-02 19:15:37 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -60,20 +60,19 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         icmpHeader = recPacket[20:28]
         type, code, checksum, id, seq = struct.unpack('bbHHh', recPacket[20:28])
         ip_header = struct.unpack('!BBHHHBBH4s4s', recPacket[:20])
-        bytesInDouble = struct.calcsize('d')
-        timeData = struct.unpack('d', recPacket[28:28 + bytesInDouble])[0]
-        delay = (timeReceived - timeData) * 1000
-        packet_min = min(packet_min, delay)
-        packet_max = max(packet_max, delay)
-        stdev_var[packet_cnt] = delay ;
-        packet_cnt += 1
-        packet_avg = (packet_min + packet_max) / 2
-        return delay
-
-
-        return delay
-
-        #return timeReceived - timeData
+        if ID == id:
+            bytesInDouble = struct.calcsize('d')
+            timeData = struct.unpack('d', recPacket[28:28 + bytesInDouble])[0]
+            delay = (timeReceived - timeData) * 1000
+            packet_min = min(packet_min, delay)
+            packet_max = max(packet_max, delay)
+            stdev_var[packet_cnt] = delay ;
+            packet_cnt += 1
+            packet_avg = (packet_min + packet_max) / 2
+            return delay
+        else:
+            delay = 0
+            return delay
 
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
