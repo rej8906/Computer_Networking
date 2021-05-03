5273c1dc79816a1cf31cb40cc1a0d56872834a16
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-05-02 21:19:40 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -129,13 +129,13 @@ def ping(host, timeout=1):
         time.sleep(1) #change me to 1
 
     #vars = [float(round(packet_min , 2)), float(round(packet_avg , 2)), float(round(packet_max , 2)), float(round((stdev(stdev_var)), 2))] # change me to 2
-    vars.append(str(round(packet_min)))
+    vars.append(float(round(packet_min)))
     #print (packet_min)
-    vars.append(str(round(packet_avg , 2)))
+    vars.append(float(round(packet_avg , 2)))
     #print(packet_avg)
-    vars.append(str(round(packet_max)))
+    vars.append(float(round(packet_max)))
     #print(packet_max)
-    vars.append(str(round((pstdev(stdev_var)) ,2 )))
+    vars.append(float(round((pstdev(stdev_var)) ,2 )))
     #print(pstdev(stdev_var))
     print(vars)
     return vars
