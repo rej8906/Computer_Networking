1d2a073d952dbe1a4233c30ccb12b719bf4ffb8e
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-05-02 22:08:49 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -130,7 +130,7 @@ def ping(host, timeout=1):
         #print(delay)
         time.sleep(1) #change me to 1
     packet_avg = sum(stdev_var) / 4
-    vars = [str(round(packet_min , 2)), str(round(packet_avg , 2)), float(round(packet_max , 2)), float(round((stdev(stdev_var)), 2))] # change me to 2
+    vars = [str(round(packet_min)), str(round(packet_avg , 2)), float(round(packet_max)), float(round((stdev(stdev_var)), 2))] # change me to 2
     #print ("avg" , packet_avg)
     #vars.append(int(round(packet_min, 2 )))
     #print (packet_min)
