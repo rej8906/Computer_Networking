346e493ee89446bcde71f09e47a2bcaf36931052
rej8906 <78665881+rej8906@users.noreply.github.com>
2021-05-02 21:38:41 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -127,12 +127,12 @@ def ping(host, timeout=1):
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
-        print(delay)
+        #print(delay)
         time.sleep(1) #change me to 1
 
     #vars = [float(round(packet_min , 2)), float(round(packet_avg , 2)), float(round(packet_max , 2)), float(round((stdev(stdev_var)), 2))] # change me to 2
     packet_avg = sum(stdev_var) / 4
-    print ("avg" , packet_avg)
+    #print ("avg" , packet_avg)
     vars.append(float(round(packet_min , 2)))
     #print (packet_min)
     vars.append(float(round(packet_avg , 2)))
